05ff32a1cdbd36c62c2ea017a93826753acc5ea3
Francis Anderson <wherlberg198@gmail.com>
2023-07-11 22:57:41 +0000

    login

---
+++ src/main/java/com/manage/inventorymanagementsystem/LoginController.java
@@ -0,0 +1,22 @@
+package com.manage.inventorymanagementsystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+
+    public TextField usernamefield;
+    public Button loginbutton;
+    public PasswordField passfield;
+    public Button cancelbutton;
+    public Label loginmessage;
+
+
+    public void oncancelbuttonclick(ActionEvent actionEvent) {
+    }
+}
